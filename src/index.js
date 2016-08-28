23889d21b442514ca0f5c0fc9c30907e8c0393d2
Joseph Thomas <joey6978@gmail.com>
2016-08-28 15:47:56 -0700

    Updated README

--- src/index.js
+++ src/index.js
@@ -1,63 +1,63 @@
 (function(window){
+  
+  var OutClickListeners = [{listener: null, exceptions: []}]
 
-var OutClickListeners = [{listener: null, exceptions: []}]
+  var addEventListener = Node.prototype.addEventListener
 
-var addEventListener = Node.prototype.addEventListener
-
-/** This handles any listener set by .onclick prototype property */
-Object.defineProperty(Node.prototype, 'onoutclick', {
-  set: function (func) {
-    OutClickListeners[0] = {
-      exceptions: [this],
-      listener: func
-    }
-
-    return func
-  }
-})
-
-/** This handles all addEventListener */
-window.Node.prototype.addEventListener = function (type, listener, exceptions) {
-  if (type == 'outclick') {
-    exceptions = exceptions || []
-    exceptions.push(this)
-
-    OutClickListeners.push({
-      exceptions: exceptions,
-      listener: listener
-    })
-  } else {
-    addEventListener.apply(this,arguments)
-  }
-}
-
-window.document.addEventListener('click', function(e){
-  for(var i = OutClickListeners.length; i--;){
-    var listener = OutClickListeners[i]
-    var contains = false
-
-    for(var g = listener.exceptions.length; g--;){
-      if (listener.exceptions[g].contains(e.target)) {
-        contains = true
-        break
+  /** This handles any listener set by .onclick prototype property */
+  Object.defineProperty(Node.prototype, 'onoutclick', {
+    set: function (func) {
+      OutClickListeners[0] = {
+        exceptions: [this],
+        listener: func
       }
 
-      if(!contains){
-        listener.listener && listener.listener(e)
-      }
+      return func
     }
-  }
-})
-
-/** This handels the HTML onclick property */
-var elements = document.querySelectorAll('[outclick]')
-
-;[].forEach.call(elements, function(e){
-  var outclick = e.getAttribute('outclick')
-  var func = Function(outclick)
-  OutClickListeners.push({
-    listener: func,
-    exceptions: [e]
   })
-})
+
+  /** This handles all addEventListener */
+  window.Node.prototype.addEventListener = function (type, listener, exceptions) {
+    if (type == 'outclick') {
+      exceptions = exceptions || []
+      exceptions.push(this)
+
+      OutClickListeners.push({
+        exceptions: exceptions,
+        listener: listener
+      })
+    } else {
+      addEventListener.apply(this,arguments)
+    }
+  }
+
+  window.document.addEventListener('click', function(e){
+    for(var i = OutClickListeners.length; i--;){
+      var listener = OutClickListeners[i]
+      var contains = false
+
+      for(var g = listener.exceptions.length; g--;){
+        if (listener.exceptions[g].contains(e.target)) {
+          contains = true
+          break
+        }
+
+        if(!contains){
+          listener.listener && listener.listener(e)
+        }
+      }
+    }
+  })
+
+  /** This handels the HTML onclick property */
+  var elements = document.querySelectorAll('[outclick]')
+
+  ;[].forEach.call(elements, function(e){
+    var outclick = e.getAttribute('outclick')
+    var func = Function(outclick)
+    OutClickListeners.push({
+      listener: func,
+      exceptions: [e]
+    })
+  })
 })(window)
